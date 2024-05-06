0c58e97cc032561ffd1b2e360b01cc5a8e1e0b74
The Stig <thestig294@gmail.com>
2024-05-06 20:43:37 +0930

    Failed attempt at adding a new setting to the maclunkey to appear as a traitor to traitors and a jester to everyone else
    Highlights cannot be overridden properly
    
    Glitch and its appearance settings makes this very complex
    
    There are other required functions from base custom roles that have to be defined whenever you are using any target override hooks from custom roles, don't know what to put in these functions
    
    ttt_maclunkey_jester_highlights 0 is close enough anyway to what was requested

--- lua/customroles/maclunkey.lua
+++ lua/customroles/maclunkey.lua
@@ -1,8 +1,9 @@
 CreateConVar("ttt_maclunkey_gun_damage", "1000", nil, "Sets the damage dealt by the Maclunkey Gun", 0, 2000)
-local jesterHighlightsCvar = CreateConVar("ttt_maclunkey_jester_highlights", "1", FCVAR_REPLICATED, "0 - Maclunkey's appearance works like a traitor, 1 - Maclunkey's appearance works like a jester until shooting the Maclunkey gun, 2 - Maclunkey's appearance works like a jester until shooting the Maclunkey gun, but appears as a traitor to fellow traitors", 0, 2)
+local highlightsCvar = CreateConVar("ttt_maclunkey_jester_highlights", "1", FCVAR_REPLICATED, "0 - Maclunkey's appearance works like a traitor, 1 - Maclunkey's appearance works like a jester until shooting the Maclunkey gun", 0, 2)
+-- local highlightsCvar = CreateConVar("ttt_maclunkey_jester_highlights", "1", FCVAR_REPLICATED, "0 - Maclunkey's appearance works like a traitor, 1 - Maclunkey's appearance works like a jester until shooting the Maclunkey gun, 2 - Maclunkey's appearance works like a jester until shooting the Maclunkey gun, but appears as a traitor to fellow traitors", 0, 2)
 local HIGHLIGHTS_TRAITOR = 0
 local HIGHLIGHTS_JESTER = 1
-local HIGHLIGHTS_MIXED = 2
+-- local HIGHLIGHTS_MIXED = 2
 local ROLE = {}
 ROLE.nameraw = "maclunkey"
 ROLE.name = "Maclunkey"
@@ -39,11 +40,11 @@ table.insert(ROLE.convars, {
     decimal = 0
 })
 
-if jesterHighlightsCvar:GetInt() == HIGHLIGHTS_JESTER then
+if highlightsCvar:GetInt() == HIGHLIGHTS_JESTER or highlightsCvar:GetInt() == HIGHLIGHTS_MIXED then
     -- Helper functions that handle all of the maclunkies' jester logic, if jester halos are enabled.
     ROLE.isactive = function(ply) return ply:GetNWBool("RevealedMaclunkey", false) end
     ROLE.shouldactlikejester = function(ply) return not ply:IsRoleActive() end
-elseif jesterHighlightsCvar:GetInt() == HIGHLIGHTS_TRAITOR then
+else
     ROLE.isactive = nil
     ROLE.shouldactlikejester = nil
 end
@@ -73,7 +74,7 @@ RunConsoleCommand("ttt_maclunkey_shop_delay", "1")
 if SERVER then
     AddCSLuaFile()
 
-    if jesterHighlightsCvar:GetInt() == HIGHLIGHTS_TRAITOR then
+    if highlightsCvar:GetInt() == HIGHLIGHTS_TRAITOR then
         -- Makes the maclunkey gun handle the jester damage, so the jester halos for the Maclunkey can be removed
         hook.Add("EntityTakeDamage", "MaclunkeyAlteredDamage", function(target, dmginfo)
             local attacker = dmginfo:GetAttacker()
@@ -129,4 +130,63 @@ if CLIENT then
             return "<p>The " .. ROLE_STRINGS[ROLE_MACLUNKEY] .. " is a member of the <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>" .. teamName .. " team</span> who pretends to be a <span style='color: rgb(" .. jesterRoleColor.r .. ", " .. jesterRoleColor.g .. ", " .. jesterRoleColor.b .. ")'>" .. jesterTeamName .. "</span>. <br>They are immune to environmental damage and cannot damage others, until they shoot their <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>" .. ROLE_STRINGS[ROLE_MACLUNKEY] .. " gun</span>.<br><br>- Appears as a " .. jesterTeamName .. " to those that can see a " .. jesterTeamName .. ", until their " .. ROLE_STRINGS[ROLE_MACLUNKEY] .. " gun is shot (including their fellow " .. teamName .. "(s)!)<br><br>- Possible to also have a " .. jesterTeamName .. " in the round</p>"
         end
     end)
+    -- local function GetGlitchedRole(target)
+    --     local glitchMode = GetConVar("ttt_glitch_mode"):GetInt()
+    --     -- Use the player's role if they are a traitor, otherwise this is a glitch and we should use their fake role
+    --     local role = target:IsTraitorTeam() and target:GetRole() or target:GetNWInt("GlitchBluff", ROLE_TRAITOR)
+    --     -- Only hide vanilla traitors
+    --     if glitchMode == GLITCH_SHOW_AS_TRAITOR then
+    --         if role == ROLE_TRAITOR then return ROLE_NONE, ROLE_TRAITOR end
+    --         -- Hide all traitors, but show whether they are special or not based on the color
+    --         return role, nil
+    --     elseif glitchMode == GLITCH_SHOW_AS_SPECIAL_TRAITOR then
+    --         return ROLE_NONE, role
+    --     end
+    --     -- Hide all traitors, period
+    --     return ROLE_NONE, ROLE_TRAITOR
+    -- end
+    -- if highlightsCvar:GetInt() == HIGHLIGHTS_MIXED then
+    --     hook.Add("TTTTargetIDPlayerRoleIcon", "MaclunkeyRoleIcon", function(target, client, role, noZ, colorRole, hideBeggar, showJester, hideBodysnatcher)
+    --         if not target:IsMaclunkey() or not client:IsTraitorTeam() then return end
+    --         if GetGlobalBool("ttt_glitch_round", false) then
+    --             role, colorRole = GetGlitchedRole(target)
+    --             return role, noZ, colorRole
+    --         else
+    --             return ROLE_MACLUNKEY, noZ, ROLE_TRAITOR
+    --         end
+    --     end)
+    --     hook.Add("TTTTargetIDPlayerRing", "MaclunkeyRoleIcon", function(target, client, ringVisible)
+    --         if not target:IsMaclunkey() or not client:IsTraitorTeam() then return end
+    --         if GetGlobalBool("ttt_glitch_round", false) then
+    --             role, colorRole = GetGlitchedRole(target)
+    --             return ringVisible, ROLE_COLORS[colorRole]
+    --         else
+    --             return ringVisible, ROLE_COLORS[ROLE_TRAITOR]
+    --         end
+    --     end)
+    --     hook.Add("TTTScoreboardPlayerRole", "MaclunkeyRoleIcon", function(target, client, color, roleFileName)
+    --         if not target:IsMaclunkey() or not client:IsTraitorTeam() then return end
+    --         if GetGlobalBool("ttt_glitch_round", false) then
+    --             role, colorRole = GetGlitchedRole(target)
+    --             return ROLE_COLORS_SCOREBOARD[colorRole], ROLE_STRINGS_SHORT[role]
+    --         else
+    --             return ROLE_COLORS_SCOREBOARD[ROLE_TRAITOR], ROLE_STRINGS_SHORT[ROLE_MACLUNKEY]
+    --         end
+    --     end)
+    --     local client
+    --     hook.Add("PreDrawHalos", "MaclunkeyHighlights", function()
+    --         if not IsValid(client) then
+    --             client = LocalPlayer()
+    --             return
+    --         end
+    --         local allies = {}
+    --         -- -- Start with the list of traitors
+    --         -- local allies = GetTeamRoles(TRAITOR_ROLES)
+    --         -- -- And add the glitch
+    --         -- table.insert(allies, ROLE_GLITCH)
+    --         -- And add the maclunkey
+    --         table.insert(allies, ROLE_MACLUNKEY)
+    --         OnPlayerHighlightEnabled(client, allies, jesters_visible_to_traitors, true, true)
+    --     end)
+    -- end
 end
